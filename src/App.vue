d7d3c5d0c23032a538aeb68f9611ac9709cb6ca8
Marc Almond Valleza <majvalleza@gmail.com>
2020-01-20 15:56:55 +0800

    feat: add drawer items

--- src/App.vue
+++ src/App.vue
@@ -20,6 +20,25 @@
       bordered
       content-class="bg-grey-2"
     )
+      div.bg-black
+        div.row.items-center
+          div.col-3.q-px-md
+            q-avatar(color="primary" text-color="white") A
+          div.col-8.q-px-sm
+            h5.text-white Almond Valleza
+          
+
+      q-list(bordered)
+        q-item(
+          clickable
+          v-ripple
+          v-for="(item, key) in drawerItems"
+          :key="key"
+        )
+          q-item-section(avatar)
+            q-icon(:name="item.icon")
+          q-item-section {{ item.title }}
+
     q-page-container
       todo-list
 </template>
@@ -36,7 +55,11 @@ export default {
 
   data () {
     return {
-      leftDrawerOpen: false
+      leftDrawerOpen: false,
+      drawerItems: [
+        { title: 'List' , icon: 'inbox' },
+        { title: 'Help', icon: 'help' }
+      ]
     }
   }
 }
