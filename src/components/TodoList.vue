2aeaa62a879886c96f8209606a041bcc01ca7da9
Marc Almond Valleza <majvalleza@gmail.com>
2020-01-20 16:39:25 +0800

    feat: create todo component

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,14 +1,93 @@
-<template>
-  <q-page class="flex flex-center">
-    <img alt="Quasar logo" src="../assets/logo.png">
-  </q-page>
+<template lang="pug">
+  q-page
+    q-input(square outlined v-model="newTodo" label="Add New Task")
+      template(v-slot:after)
+        q-btn(round dense icon="add" color="primary" @click="addTask")
+    q-separator
+    q-list(bordered)
+      q-item(
+        clickable
+        v-ripple
+        v-for="(todo, key) in todos"
+        :key="key"
+        :active="todo.isCompleted"
+        active-class="completed-task"
+      )
+        q-item-section(side top)
+          q-checkbox(v-model="todo.isCompleted" @input="completeTask(todo.isCompleted)")
+        q-item-section
+          q-item-label {{ todo.title }}
+        q-item-section(v-if="todo.isCompleted" side top)
+          q-btn(
+            flat
+            dense
+            round
+            icon="delete"
+            color="negative"
+            @click="deleteTask(key)"
+          )
+    //- q-page-sticky(position="bottom-right").q-pa-lg
+    //-   q-btn(fab icon="add" color="primary" @click="showAddTask")
+
+      
+    
 </template>
 
-<style>
-</style>
 
 <script>
 export default {
-  name: 'HelloWorld'
+  name: 'TodoList',
+
+  data () {
+    return {
+      newTodo: '',
+      todos: [
+        { 
+          title: 'Clean the house',
+          isCompleted: false
+        },
+        {
+          title: 'Feed the dogs',
+          isCompleted: false
+        }
+        
+      ]
+    };
+  },
+  methods: {
+    addTask () {
+      let task = {
+        title: this.newTodo,
+        isCompleted: false
+      };
+      this.todos.push(task);
+      this.newTodo = '';
+      this.$q.notify({
+        message: 'Successfully added new task!',
+        color: 'positive'
+      });
+    },
+    completeTask (isCompleted) {
+      if (isCompleted) {
+        this.$q.notify({
+          message: 'Task is completed!',
+          color: 'primary'
+        });
+      }
+    },
+    deleteTask(index) {
+      this.todos.splice(index, 1);
+      this.$q.notify({
+        message: 'Task is successfully deleted!',
+        color: 'positive'
+      });
+    }
+  }
 }
 </script>
+
+<style scoped>
+.completed-task {
+  background: #c8e4f7;
+}
+</style>
